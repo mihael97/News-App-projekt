4565985191fd9e68035de14fd3e5659c874de0b5
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-05-20 21:37:02 +0200

    Initial commit

--- news-app/view/InitialController.swift
+++ news-app/view/InitialController.swift
@@ -1,20 +1,17 @@
 //
-//  ViewController.swift
+//  InitialController.swift
 //  news-app
 //
 //  Created by five on 20/05/2021.
 //  Copyright © 2021 ios-vjestina. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class InitialController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
     }
-
-
 }
-
