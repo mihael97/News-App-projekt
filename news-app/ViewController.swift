8f00d8e91220abdb711b5644ac72cd9bcf6fa601
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-05-20 21:26:27 +0200

    Initial Commit

---
+++ news-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news-app
+//
+//  Created by five on 20/05/2021.
+//  Copyright © 2021 ios-vjestina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
