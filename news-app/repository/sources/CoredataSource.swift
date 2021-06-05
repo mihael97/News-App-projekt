4e7a21b492c7565e8dd3678746ff931e40ac273b
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-06-05 22:44:04 +0200

    Fetching by category

---
+++ news-app/repository/sources/CoredataSource.swift
@@ -0,0 +1,27 @@
+//
+//  CoredataSource.swift
+//  news-app
+//
+//  Created by five on 05/06/2021.
+//  Copyright © 2021 ios-vjestina. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataSource {
+    public func getHistoryArticles()->[Article] {
+        return []
+    }
+    
+    public func addToHistory(article: Article) {
+        
+    }
+    
+    public func getSavedArticles()->[Article] {
+        return []
+    }
+    
+    public func saveArticle(article: Article) {
+        
+    }
+}
